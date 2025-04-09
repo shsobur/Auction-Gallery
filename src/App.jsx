e1f88940c4e81d60f43a0ea15773fdfa48ac1d65
shsobur <shsoburhossen951@gmail.com>
2025-04-09 20:17:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <section>
+        <Navbar></Navbar>
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import { FaRegBell } from "react-icons/fa";
+import "./Navbar.css";
+import { CgProfile } from "react-icons/cg";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="main_navbar_top_container">
+
+          <div className="navbar_logo_container">
+            <h1>
+              Auction<span>Galleary</span>
+            </h1>
+          </div>
+
+          <div className="navbar_navigation_container">
+            <ul>
+              <li>Home</li>
+              <li>Auctions</li>
+              <li>Categories</li>
+              <li>How to works</li>
+            </ul>
+          </div>
+
+          <div className="navbar_user_profile_container">
+            <span>
+              <FaRegBell />
+            </span>
+            <span>
+              <CgProfile />
+            </span>
+          </div>
+
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
